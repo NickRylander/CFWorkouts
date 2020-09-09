4aae812de26f65526a7979901028f4aa2980ce74
NickRylander <nick.rylander@outlook.com>
2020-09-09 16:58:39 -0700

    bare bones month and day class

--- lib/day.rb
+++ lib/day.rb
@@ -1,3 +1,14 @@
 class Day
 
+    @@all = []
+    attr_accessor :day
+
+    def initialize(day)
+        @day = day
+        @@all << self
+    end
+    def self.all
+        @@all
+    end
+
 end
--- lib/month.rb
+++ lib/month.rb
@@ -1,3 +1,13 @@
 class Month
+    @@all = []
+    attr_accessor :month
+
+    def initialize(month)
+        @month = month
+        @@all << self
+    end
+    def self.all
+        @@all
+    end
 
 end
