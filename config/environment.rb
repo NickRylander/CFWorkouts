f014174228738f1c81691a7b2d1f8b3de6442097
NickRylander <nick.rylander@outlook.com>
2020-09-09 17:02:08 -0700

    added config/environment instead of putting it in bin/run

---
+++ config/environment.rb
@@ -0,0 +1,8 @@
+require "nokogiri"
+require "open-uri"
+require "pry"
+
+require_relative '../lib/cli'
+require_relative '../lib/scraper'
+require_relative '../lib/month'
+require_relative '../lib/day'
